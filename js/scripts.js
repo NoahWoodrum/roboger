a1d834302407200591157c3290c5b84fd7791e0d
Noah Woodrum <heymymomthinksimcool@gmail.com>
2021-04-02 14:32:59 -0700

    update the js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,24 @@
+let numberArray = []
+
+//buisness logic
 function translator(number) {
-  const numberArray = ["0", "beep", "boop", "wont you be my neighbor", "4", "5"]
-  if (number === 13) {
-    return "wont you be my neighbor"
-  } else if (number === 21) {
-    return "boop"
-  } else if (number === 32) {
-    return "wont you be my neighbor"
-  } 
+  let numberInt = parseInt(number);
+  for (var number i = 0; i <= numberInt; i+=1);
+  const numbers = (""+index).split("")
+  if (index === 3 || numbers.includes("3")) {
+    outputArray.push("Won't your be my neighbor?")
+    continue
+  }
+  //let numberArray = ["0", "beep", "boop", "wont you be my neighbor", "4", "5"];
+
+  //if (number === 13) {
+    //return "wont you be my neighbor"
+  //} else if (number === 21) {
+    //return "boop"
+  //} else if (number === 32) {
+   // return "wont you be my neighbor"
+  //} 
+
 
 
 }
